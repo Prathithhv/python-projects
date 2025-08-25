01054dc577b5e6fba2f27c421e1ce63d82263c3e
prathith <prathithhv@gmail.com>
2025-08-26 01:49:11 +0530

    Add files via upload

---
+++ years left in life.py
@@ -0,0 +1,6 @@
+age=int(input("what is your age?\n"))
+years_remaining=90-age
+months_remaining=years_remaining*12
+weeks_remaining=years_remaining*52
+days_remaining=years_remaining*365
+print(f"you have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left")
