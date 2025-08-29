63087739d5e5f7cd387d2c1b7a18aef2ba4fa4f0
prathith <prathithhv@gmail.com>
2025-08-30 00:29:03 +0530

    Update heads or tails.py

--- heads or tails.py
+++ heads or tails.py
@@ -1,6 +1,8 @@
 
 import random
 choice=input('heads or tails?\n').lower()
-if choice == "heads" or choice == "tails":
+while choice not in ("heads", "tails"):
+ print("invalid input. please choose heads or tails")
+ choice=input('heads or tails?\n').lower()
  random_choice=random.choice(("heads" ,"tails"))
 print(random_choice)
