ac2f0a49ac25e4ed896ab8e1432e131ddd86993f
prathith <prathithhv@gmail.com>
2025-08-30 00:31:20 +0530

    Update heads or tails.py

--- heads or tails.py
+++ heads or tails.py
@@ -1,4 +1,3 @@
-
 import random
 choice=input('heads or tails?\n').lower()
 while choice not in ("heads", "tails"):
@@ -7,4 +6,6 @@ while choice not in ("heads", "tails"):
  if choice=="heads" or choice="tails:
  random_choice=random.choice(("heads" ,"tails"))
 print(random_choice)
+
+
 
