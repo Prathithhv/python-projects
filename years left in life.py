02c7aff0da00f36c4a76205030cd7fce71c62ce8
prathith <prathithhv@gmail.com>
2025-10-19 08:54:56 +0530

    Add clarification to life expectancy message

--- years left in life.py
+++ years left in life.py
@@ -3,4 +3,5 @@ years_remaining=90-age
 months_remaining=years_remaining*12
 weeks_remaining=years_remaining*52
 days_remaining=years_remaining*365
-print(f"you have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left")
+
+print(f"Taking an assumption that 90 years is your life span you have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left")
