60cec4058ea194c62304e714f7c66684324b1923
prathith <prathithhv@gmail.com>
2026-05-21 20:42:45 +0530

    Refactor Caesar cipher with loop and improved logic
    Refactor Caesar cipher implementation for clarity and functionality. Added a loop for repeated encoding/decoding.

--- ceaser_cypher.py
+++ ceaser_cypher.py
@@ -10,37 +10,46 @@ print(''' welcome to ceaser cypher  ______                                      
                                                                                 | $$                                    
                                                                                 | $$                                    
                                                                                  \$$   ''')
-alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction=input("type 'encode' if you want to encrypt and type 'decode' if you want to decrypt\n").lower()
-text=input("enter the word you want to encode or decode\n").lower()
-shift=int(input("enter the number by which you want to shift the letters\n"))
-
-def encrypt(original_text=text,shifted_number=shift):
-    cypher_text=""
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+direction=input("type 'encode' to encrypt and 'decode' to decrypt\n").lower()
+text=input(f"enter text to {direction}\n").lower()
+shift=int(input("enter the number by which the text is to be shifted\n"))
+def encrypt(original_text,shift_amount):
+    cipher_text=""
     for letter in original_text:
-        if letter not in alphabets:
-            cypher_text+=letter
-        else:    
-            shifted_position=(alphabets.index(letter) +shifted_number)%26
-            new_letter=alphabets[shifted_position]
-            cypher_text+=new_letter
-    print(f"the encoded word is {cypher_text}")
-def decrypt (original_text=text,shifted_number=shift):
-    cypher_text=""
-    for letter in original_text:
-         if letter not in alphabets:
-            cypher_text+=letter
-         else:   
-            shifted_position=(alphabets.index(letter) -shifted_number)%26
-            new_letter=alphabets[shifted_position]
-            cypher_text+=new_letter
-    print(f"the decoded word is {cypher_text}")
-
-
-
+        if letter not in alphabet:
+            cipher_text+=letter
+        else:
+             shifted_position=alphabet.index(letter) + shift_amount
+             shifted_position %= len(alphabet)
+             cipher_text+=alphabet[shifted_position]
+    print(f"here is the encoded result:{cipher_text}")   
+   
+def decrypt(original_text,shift_amount):
+     cipher_text=""
+     for letter in original_text:
+        if letter not in alphabet:
+            cipher_text+=letter
+        else:
+            shifted_position=alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text+=alphabet[shifted_position]
+     print(f"here is the decoded result:{cipher_text}")
 if direction=="encode":
-    encrypt(original_text=text,shifted_number=shift)
+    encrypt(original_text=text,shift_amount=shift)
 elif direction=="decode":
-    decrypt(original_text=text,shifted_number=shift)
-
-
+    decrypt(original_text=text,shift_amount=shift)
+continue1=True
+while continue1:
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    direction=input("type 'encode' to encrypt and 'decode' to decrypt\n").lower()
+    text=input(f"enter text to {direction}\n").lower()
+    shift=int(input("enter the number by which the text is to be shifted\n"))
+    if direction=="encode":
+        encrypt(original_text=text,shift_amount=shift)
+    elif direction=="decode":
+        decrypt(original_text=text,shift_amount=shift)
+    choice=input("do u want to go again?").lower()
+    if choice=="no":
+     continue1=False
+     print("goodbye")
